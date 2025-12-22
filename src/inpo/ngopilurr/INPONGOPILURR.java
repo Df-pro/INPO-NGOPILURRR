675a9f30e5df40f8c99a37b904bc87edd8d80513
FERDI <dwiferdianto69@gmail.com>
2025-12-22 10:58:05 +0700

    contoh

--- src/inpo/ngopilurr/INPONGOPILURR.java
+++ src/inpo/ngopilurr/INPONGOPILURR.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package inpo.ngopilurr;
-
+import java.util.Scanner;
 /**
  *
  * @author dwife
@@ -16,6 +16,26 @@ public class INPONGOPILURR {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nama: ");
+        String nama = input.nextLine();
+
+        System.out.print("Masukkan umur: ");
+        int umur = input.nextInt();
+
+        System.out.println("\n=== HASIL ===");
+        System.out.println("Nama  : " + nama);
+        System.out.println("Umur  : " + umur + " tahun");
+
+        if (umur >= 18) {
+            System.out.println("Status: Dewasa");
+        } else {
+            System.out.println("Status: Belum dewasa");
+        }
+
+        input.close();
     }
     
 }
